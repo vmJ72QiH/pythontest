b32a5cc130e6ac2d73fd9b5d32fd5dc7eae6fec5
vmJ72QiH <52655474+vmJ72QiH@users.noreply.github.com>
2020-02-12 15:02:56 +0800

    Update bbb.py

--- 打印a文件包含b文件行内容的行/bbb.py
+++ 打印a文件包含b文件行内容的行/bbb.py
@@ -8,4 +8,4 @@ with open("b.txt") as f:
        			for k in ff:
 				if k.replace('\n','') in i.replace('\n',''):
        					with open("dd.txt", 'a') as fff:
-                            fff.write(i)
+                            			fff.write(i)
